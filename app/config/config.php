c2e29d47a2a88283c93364c86039838818d4aa34
alexandru.banut <alexandru.banut@emag.ro>
2022-09-09 17:01:44 +0300

    MVC Framework

---
+++ app/config/config.php
@@ -0,0 +1,18 @@
+<?php
+
+//DB Params
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'YOUR_USER');
+define('DB_PASS', 'YOUR_PASS');
+define('DB_NAME', 'YOUR_DBNAME');
+
+// App Root
+define('APPROOT', dirname(dirname(__FILE__)));
+
+// URL Root
+define('URLROOT', 'YOUR_URL');
+
+//Site Name
+define('SITENAME', 'YOUR_SITE_NAME');
+
